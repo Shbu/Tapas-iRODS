d6c2e0492e59e866876ab6e1e807e9564eebb2c0
Sharan Babu K <sharankbabu@gmail.com>
2013-11-18 17:08:36 -0700

    Fixed iRODS Zone problem.

--- irods-java-file-io-application/src/main/java/org/bio5/irods/imagej/views/DirectoryContentsPane.java
+++ irods-java-file-io-application/src/main/java/org/bio5/irods/imagej/views/DirectoryContentsPane.java
@@ -2,17 +2,13 @@ package org.bio5.irods.imagej.views;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
 import java.util.List;
 
 import javax.imageio.ImageIO;
@@ -24,6 +20,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.LineBorder;
@@ -34,17 +31,10 @@ import org.bio5.irods.imagej.fileoperations.FileOperations;
 import org.irods.jargon.core.connection.IRODSAccount;
 import org.irods.jargon.core.exception.FileNotFoundException;
 import org.irods.jargon.core.exception.JargonException;
-import org.irods.jargon.core.pub.IRODSFileSystem;
-import org.irods.jargon.core.pub.UserAO;
-import org.irods.jargon.core.pub.io.FileIOOperations;
-import org.irods.jargon.core.pub.io.FileIOOperations.SeekWhenceType;
 import org.irods.jargon.core.pub.io.IRODSFile;
 import org.irods.jargon.core.pub.io.IRODSFileFactory;
 import org.irods.jargon.core.pub.io.IRODSFileInputStream;
-import org.irods.jargon.core.pub.io.IRODSFileReader;
-
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import javax.swing.JScrollBar;
 
 public class DirectoryContentsPane extends JPanel {
 
@@ -54,15 +44,18 @@ public class DirectoryContentsPane extends JPanel {
 	private static final long serialVersionUID = 722996165620904921L;
 	private JTree userDirectoryTree;
 	private DefaultMutableTreeNode rootNode;
+	private String irodsZone;
 	/**
 	 * Create the panel.
 	 * @throws JargonException 
 	 */
+	
 	public DirectoryContentsPane(List<String> ContentsInHome,final IRODSFile irodsAccountFile,final IRODSAccount irodsAccount) throws JargonException {
 
 		final IRODSFileFactory iRODSFileFactory = FileOperations.getIrodsAccountFileFactory(irodsAccount);
 		String irodsZone =irodsAccount.getZone();
-		
+		System.out.println("irodsZone" +irodsZone);
+
 		rootNode = new DefaultMutableTreeNode("home");
 		/*Iterator<String> itr=ContentsInHome.iterator();
 		while(itr.hasNext())
@@ -72,71 +65,66 @@ public class DirectoryContentsPane extends JPanel {
 		}*/
 
 		final File localFiles = (File) irodsAccountFile;
-		parseDirectoryContents(iRODSFileFactory, localFiles, rootNode);
-		
+		parseDirectoryContents(iRODSFileFactory, localFiles, rootNode, irodsAccount);
+
 		userDirectoryTree.setToolTipText("Directory list");
 		userDirectoryTree.setVisibleRowCount(50);
 		userDirectoryTree.setBorder(new LineBorder(new Color(0, 0, 0)));
-		
+
 		JButton btnNewButton_AddToImageJ = new JButton("Add to ImageJ");
-		
+
 		JButton btnNewButton_OpenImage = new JButton("Open Image");
 		btnNewButton_OpenImage.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try{
-				//IRODSFileSystem irodsFileSystem= IRODSFileSystem.instance();
-				//UserAO  userAccount = irodsFileSystem.getIRODSAccessObjectFactory().getUserAO(irodsAccount);
-				
-				
-				//IRODSFileReader iofileReader = new IRODSFileReader(irodsAccountFile, iRODSFileFactory);
-				
-				IRODSFile[] direcFiles = (IRODSFile[]) irodsAccountFile.listFiles();
-				/*IRODSFile dirFile =null;
+					//IRODSFileSystem irodsFileSystem= IRODSFileSystem.instance();
+					//UserAO  userAccount = irodsFileSystem.getIRODSAccessObjectFactory().getUserAO(irodsAccount);
+					//IRODSFileReader iofileReader = new IRODSFileReader(irodsAccountFile, iRODSFileFactory);
+
+					IRODSFile[] direcFiles = (IRODSFile[]) irodsAccountFile.listFiles();
+					/*IRODSFile dirFile =null;
 				for(int i=0; i<direcFiles.length;i++)
 				{
 					dirFile=direcFiles[i];
 					if(dirFile.isFile())
 					break;	
 				}*/
-				System.out.println("URI" +irodsAccountFile.getAbsolutePath());
-				IRODSFileInputStream irodsfileistream = iRODSFileFactory.instanceIRODSFileInputStream(irodsAccountFile.getAbsolutePath() +"/TestImages/SpitzerTelescope_PinWheelGalaxy.jpg");
-				
-				
-				BufferedImage bi = ImageIO.read(irodsfileistream);
+					System.out.println("Absolute Path: " +irodsAccountFile.getAbsolutePath());
+					IRODSFileInputStream irodsfileistream = iRODSFileFactory.instanceIRODSFileInputStream(irodsAccountFile.getAbsolutePath() +"/TestImages/SpitzerTelescope_PinWheelGalaxy.jpg");
 
-				JFrame frame = new JFrame();  
-		        JLabel label = new JLabel(new ImageIcon(bi));  
-		        frame.getContentPane().add(label, BorderLayout.CENTER);  
-		        frame.pack();  
-		        frame.setVisible(true); 
-		        
+					BufferedImage bi = ImageIO.read(irodsfileistream);
+					JFrame frame = new JFrame();  
+					JLabel label = new JLabel(new ImageIcon(bi));  
+					frame.getContentPane().add(label, BorderLayout.CENTER);  
+					frame.pack();  
+					frame.setVisible(true); 
+
 				}
 				catch (Exception ex){
 					ex.printStackTrace();
 				}
-				 
 			}
 		});
-		
+
 		JButton btnHome = new JButton("Home");
 		btnHome.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					List<String> dirList= FileOperations.getDirectoryContents(irodsAccount, null);
-					
+
 					IRODSFile irodsAccountFile =FileOperations.getIrodsAccountFile(irodsAccount);
 					DirectoryContentsPane directoryContents  =new DirectoryContentsPane(dirList,irodsAccountFile,irodsAccount);
 					MainWindow mw  =new MainWindow();
 					mw.setContentPane(directoryContents);
 				} catch (FileNotFoundException e1) {
 					JOptionPane.showMessageDialog(null, "Directory doesnt exists", "Not Home directory", 1);
-					
+
 					e1.printStackTrace();
 				} catch (JargonException e1) {
 					JOptionPane.showMessageDialog(null, "Jargon Exception", "Jargon Exception", 1);
 					e1.printStackTrace();
 				}
-				
+
 			}
 		});
 		GroupLayout groupLayout = new GroupLayout(this);
@@ -144,7 +132,8 @@ public class DirectoryContentsPane extends JPanel {
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(userDirectoryTree, GroupLayout.DEFAULT_SIZE, 264, Short.MAX_VALUE)
+					.addComponent(userDirectoryTree, GroupLayout.DEFAULT_SIZE, 394, Short.MAX_VALUE)
+					.addGap(10)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addGap(18)
@@ -154,7 +143,7 @@ public class DirectoryContentsPane extends JPanel {
 						.addGroup(groupLayout.createSequentialGroup()
 							.addPreferredGap(ComponentPlacement.UNRELATED)
 							.addComponent(btnHome)))
-					.addContainerGap(136, Short.MAX_VALUE))
+					.addGap(33))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
@@ -170,32 +159,12 @@ public class DirectoryContentsPane extends JPanel {
 					.addContainerGap())
 		);
 		setLayout(groupLayout);
-
 	}
-	
-	void parseDirectoryContents(final IRODSFileFactory iRODSFileFactory,final File irodsAccountFile, DefaultMutableTreeNode node)
-	{
-		
-		/*File[] f =irodsAccountFile.listFiles();
-		for(int i=0; i<f.length;i++)
-		{
-			if(f[i].isDirectory()){
-				node.add(new DefaultMutableTreeNode(f[i].getName()));
-				System.out.println(f[i].getName() +node.getDepth());
-				parseDirectoryContents(f[i],new DefaultMutableTreeNode(f[i].getName()));
-			}
-			else{
-				Need to update code to display leaf nodes in a dir -- currently check console o/p 
-				System.out.println(f[i].getName());
-				DefaultMutableTreeNode parentNode= new DefaultMutableTreeNode(f[i].getParent());
-				parentNode.add(new DefaultMutableTreeNode(f[i].getName()));
-			}
-		}*/
-		
-		
-		if(!irodsAccountFile.isDirectory()){
 
-			System.out.println("File name" +irodsAccountFile.getName() +irodsAccountFile.getAbsolutePath());
+	void parseDirectoryContents(final IRODSFileFactory iRODSFileFactory,final File irodsAccountFile, DefaultMutableTreeNode node, final IRODSAccount irodsAccount)
+	{
+		if(!irodsAccountFile.isDirectory()){
+			System.out.println("File name" +irodsAccountFile.getName() +":" +irodsAccountFile.getAbsolutePath());
 			DefaultMutableTreeNode child = new DefaultMutableTreeNode(irodsAccountFile.getName());
 			node.add(child);
 		}
@@ -206,19 +175,20 @@ public class DirectoryContentsPane extends JPanel {
 			File[] direcFiles=irodsAccountFile.listFiles();
 			for(int i=0;i<direcFiles.length;i++){
 				System.out.println("File number" +i);
-				parseDirectoryContents(iRODSFileFactory, direcFiles[i], child);
+				parseDirectoryContents(iRODSFileFactory, direcFiles[i], child, irodsAccount);
 			}
 		}
-		
+
 		userDirectoryTree= new JTree(rootNode);
 		userDirectoryTree.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				
+
 				if(e.getClickCount()==2){
-					String treePath=irodsZone +getJtreeSelection(e);
+					String treePath=getJtreeSelection(e);
+					System.out.println("tree path after double click" +treePath);
 					try {
-						pullFiles(iRODSFileFactory, treePath);
+						getImageFile(iRODSFileFactory, treePath,irodsAccount);
 					} catch (JargonException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
@@ -229,41 +199,42 @@ public class DirectoryContentsPane extends JPanel {
 				}
 			}
 		});
-		
+
 		userDirectoryTree.setShowsRootHandles(true);
 		userDirectoryTree.setEditable(true);
 		add(userDirectoryTree);
 	}
-	
+
 	String getJtreeSelection(MouseEvent me)
 	{
 		String fullTreePath="";
 		TreePath tp =userDirectoryTree.getPathForLocation(me.getX(), me.getY());
 		if(tp!=null)
 		{
-			
 			Object treepath[] =tp.getPath();
 			for(int i=0;i<treepath.length;i++)
 			{
 				fullTreePath  += "/" +treepath[i].toString();
 			}
-			
 		}
-			
 		return fullTreePath;
 	}
-	
-	void pullFiles(IRODSFileFactory iRODSFileFactory,String treePath) throws JargonException, IOException
+
+	void getImageFile(IRODSFileFactory iRODSFileFactory,String treePath,IRODSAccount irodsAccount) throws JargonException, IOException
 	{
-		
 		System.out.println("finalTreePath:" +treePath);
-		IRODSFileInputStream irodsfileistream = iRODSFileFactory.instanceIRODSFileInputStream(treePath);
 		
-		BufferedImage bi = ImageIO.read(irodsfileistream);
-		JFrame frame = new JFrame();  
-        JLabel label = new JLabel(new ImageIcon(bi));  
-        frame.getContentPane().add(label, BorderLayout.CENTER);  
-        frame.pack();  
-        frame.setVisible(true); 
+		/*Remove hardCoded iRODSZone*/
+		IRODSFileInputStream irodsfileistream = iRODSFileFactory.instanceIRODSFileInputStream("/" +irodsAccount.getZone() +treePath);
+		BufferedImage bufferImageIrodsFile = ImageIO.read(irodsfileistream);
+		JFrame frame = new JFrame();
+		JLabel label = new JLabel(new ImageIcon(bufferImageIrodsFile)); 
+		JScrollPane scrollPane = new JScrollPane(label);  
+		  scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);  
+		  scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED); 
+		frame.getContentPane().add(scrollPane,BorderLayout.CENTER);
+		frame.getContentPane().add(label, BorderLayout.CENTER); 
+		frame.pack();
+		frame.setVisible(true); 
 	}
 }
