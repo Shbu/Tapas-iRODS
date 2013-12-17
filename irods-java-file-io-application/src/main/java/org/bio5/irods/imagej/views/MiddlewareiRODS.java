deff8694d50e88c18f926e8f4f761a990116c78e
Sharan Babu K <sharankbabu@gmail.com>
2013-12-17 16:35:36 -0700

    Added middleware file to interact with irods from main window

---
+++ irods-java-file-io-application/src/main/java/org/bio5/irods/imagej/views/MiddlewareiRODS.java
@@ -0,0 +1,63 @@
+package org.bio5.irods.imagej.views;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.bio5.irods.imagej.connection.IrodsConnection;
+import org.irods.jargon.core.connection.IRODSAccount;
+
+public class MiddlewareiRODS extends JFrame{
+
+
+	private String username;
+	private String password;
+	private String port;
+	private String host;
+	private String zone;
+
+	private JPanel contentPane;
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPort() {
+		return port;
+	}
+	public void setPort(String port) {
+		this.port = port;
+	}
+	public String getHost() {
+		return host;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public String getZone() {
+		return zone;
+	}
+	public void setZone(String zone) {
+		this.zone = zone;
+	}
+
+	
+	/*Yet to update functionality once we resolve the ClassNotFound error*/
+	public void connection(){
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		JOptionPane.showMessageDialog(null, username);
+		IRODSAccount irodsAccount= IrodsConnection.irodsConnection(username, password, zone, host, Integer.parseInt(port));
+	}
+
+}
